a04c01dc7dd829b2b9afdac1214ff3a2285c5ef1
Michael Malick <malickmj@gmail.com>
2015-04-20 13:08:28 -0700

    Removed three graphics from coda_diag()
    I removed the gelman plot, cumuplot, and histogram from the coda_diag() function
    because they caused the function to run too slow for larger models with many
    parameters.

--- R/coda_diag.R
+++ R/coda_diag.R
@@ -1,10 +1,10 @@
 #' @title Create multiple diagnostic graphics for a single parameter
 #'
 #' @description
-#'      \code{coda_diag} creates a graphic that includes six diagnostic plots to
-#'      help monitor convergence of an MCMC chain for a parameter. By default
+#'      \code{coda_diag} creates a graphic that includes three diagnostic plots
+#'      to help monitor convergence of an MCMC chain for a parameter. By default
 #'      the function will loop through all parameters in an mcmc.list object and
-#'      create a single graphic of six diagnostic plots for each parameter.
+#'      create a single graphic of three diagnostic plots for each parameter.
 #'
 #' @param coda.object
 #'      an mcmc.list object
@@ -14,23 +14,16 @@
 #'      none are supplied all monitored parameters are included.
 #'
 #' @return
-#'      A graphic with six diagnostic plots:
+#'      A graphic with three diagnostic plots:
 #'
 #'     \itemize{
 #'         \item{traceplot of all chains}
 #'         \item{density plot of marginal distribution for each chain}
 #'         \item{autocorrelation for each chain with effective sample size 
 #'               (ess)}
-#'         \item{evolution of the Gelman-Rubin statistic with 97.5\% credibility
-#'               interval}
-#'         \item{evolution of the median value for the parameter with 95\%
-#'               credibility interval}
-#'         \item{histogram of the marginal distribution of all chains
-#'               combined with median value and 95\% credibility interval}
 #'      }
 #'
 #' @seealso \code{\link{traceplot}} \code{\link{density}}
-#'          \code{\link{gelman.plot}} \code{\link{cumuplot}} \code{\link{hist}}  
 #' 
 #' @export
 #'
@@ -71,7 +64,11 @@ coda_diag <- function(
     pal <- hcl(h = ang, c = 100, l = 60, fixup = TRUE)
 
     n.parms <- length(parms)
-    par(mfrow = c(3, 2), mar = c(4,4.5,3,0), oma = c(1,0,0,1))
+    par(mar = c(4,4.5,3,0), oma = c(1,0,0,1))
+    m <- rbind(
+        c(1,1),
+        c(2, 3))
+    layout(m)
 
     for(i in 1:n.parms) {
 
@@ -135,95 +132,97 @@ coda_diag <- function(
         axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
         axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
         abline(h = 0, col = "grey50", lty = 2)
-        ess <- coda::effectiveSize(coda.object)[parms[i]]
-        text(x = max(acor.x), y = max(acor.y),
-            labels = paste("ESS =", round(ess, 1)), adj = c(1.25, 2.5))
+        # ess <- coda::effectiveSize(coda.object)[parms[i]]
+        # text(x = max(acor.x), y = max(acor.y),
+        #     labels = paste("ESS =", round(ess, 1)), adj = c(1.25, 2.5))
 
 
         ## Gelman-Rubin Statistic
-        gp <- coda::gelman.plot(coda.object[ , parms[i]], 
-            main = "",
-            ylab = "Shrink factor",
-            xlab = "Last iteration in chain",
-            col  = c("steelblue", "grey40"),
-            lwd  = c(2, 1),
-            axes = FALSE,
-            auto.layout = FALSE)
-        axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
-        axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
-        abline(h = 1, col = "grey50", lwd = 1)
-        box(col = "grey50")
-        xx <- coda::gelman.diag(coda.object)
-        xx <- round(xx$psrf[i, 1], 3)
-        txt <- paste("Rhat =", xx)
-        mtext(txt, side = 3, cex = 0.7)
+        # gp <- coda::gelman.plot(coda.object[ , parms[i]], 
+        #     main = "",
+        #     ylab = "Shrink factor",
+        #     xlab = "Last iteration in chain",
+        #     col  = c("steelblue", "grey40"),
+        #     lwd  = c(2, 1),
+        #     axes = FALSE,
+        #     auto.layout = FALSE)
+        # axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
+        # axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
+        # abline(h = 1, col = "grey50", lwd = 1)
+        # box(col = "grey50")
+        # xx <- coda::gelman.diag(coda.object)
+        # xx <- round(xx$psrf[i, 1], 3)
+        # txt <- paste("Rhat =", xx)
+        # mtext(txt, side = 3, cex = 0.7)
         
+
+
         ## Cumulative Quantiles
-        n.iter <- dim(sim.mat)[1]
-        probs <- c(0.025, 0.50, 0.975)
-        cum.arr <- array(NA, dim = c(n.iter, length(probs), n.chains))
-        for(j in 1:n.chains) {
-            for(k in 1:n.iter) 
-                cum.arr[k , , j] <- quantile(sim.mat[1:k, j], probs = probs)
-        }
+        # n.iter <- dim(sim.mat)[1]
+        # probs <- c(0.025, 0.50, 0.975)
+        # cum.arr <- array(NA, dim = c(n.iter, length(probs), n.chains))
+        # for(j in 1:n.chains) {
+        #     for(k in 1:n.iter) 
+        #         cum.arr[k , , j] <- quantile(sim.mat[1:k, j], probs = probs)
+        # }
 
-        for(j in 1:n.chains) {
+        # for(j in 1:n.chains) {
 
-            if(j > 1)
-                add <- TRUE
-            else
-                add <- FALSE
+        #     if(j > 1)
+        #         add <- TRUE
+        #     else
+        #         add <- FALSE
 
-            matplot(as.vector(time(coda.object)), cum.arr[ , , j],
-                type = "l",
-                lwd = c(1, 2, 1),
-                # cex  = 0.7,
-                ylim = c(min(cum.arr), max(cum.arr)),
-                lty  = c(2, 1, 2),
-                col  = pal[j],
-                ylab = "Median",
-                xlab = "Iteration",
-                add  = add,
-                axes = FALSE)
+        #     matplot(as.vector(time(coda.object)), cum.arr[ , , j],
+        #         type = "l",
+        #         lwd = c(1, 2, 1),
+        #         # cex  = 0.7,
+        #         ylim = c(min(cum.arr), max(cum.arr)),
+        #         lty  = c(2, 1, 2),
+        #         col  = pal[j],
+        #         ylab = "Median",
+        #         xlab = "Iteration",
+        #         add  = add,
+        #         axes = FALSE)
 
-            if(j == 1) {
-                box(col = "grey50")
-                axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
-                axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
-            }
-        }
+        #     if(j == 1) {
+        #         box(col = "grey50")
+        #         axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
+        #         axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
+        #     }
+        # }
 
         ## Histogram
-        dat.hist <- sim.dat[ , i]
-        dens     <- density(dat.hist)
-        y.min    <- min(dens$y)
-        y.max    <- max(dens$y)
-        x.min    <- min(dens$x)
-        x.max    <- max(dens$x)
+        # dat.hist <- sim.dat[ , i]
+        # dens     <- density(dat.hist)
+        # y.min    <- min(dens$y)
+        # y.max    <- max(dens$y)
+        # x.min    <- min(dens$x)
+        # x.max    <- max(dens$x)
 
-        hist(dat.hist,
-            freq   = FALSE,
-            col    = "grey60",
-            border = "white",
-            ylab   = "Density",
-            xlab   = "Value",
-            main   = "",
-            ylim   = c(y.min, y.max),
-            xlim   = c(x.min, x.max),
-            axes   = FALSE)
-        box(col = "grey50")
-        axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
-        axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
-        lines(dens, col = "grey30")
-        rug(dat.hist, col = "grey75")
-        segments( y0 = 0, y1 = 0, 
-            x0 = quantile(dat.hist, probs = c(0.025, 0.975))[1],
-            x1 = quantile(dat.hist, probs = c(0.025, 0.975))[2],
-            lwd = 3, col = "tomato", lend = 2)
-        points(x = median(dat.hist), y = 0, pch = "|", cex = 3, 
-            col = "steelblue")
-        txt <- paste("median =", round(median(dat.hist), 3))
-        mtext(txt, side = 3, cex = 0.7)
+        # hist(dat.hist,
+        #     freq   = FALSE,
+        #     col    = "grey60",
+        #     border = "white",
+        #     ylab   = "Density",
+        #     xlab   = "Value",
+        #     main   = "",
+        #     ylim   = c(y.min, y.max),
+        #     xlim   = c(x.min, x.max),
+        #     axes   = FALSE)
+        # box(col = "grey50")
+        # axis(1, lwd = 0, col = "grey50", lwd.ticks = 1)
+        # axis(2, lwd = 0, col = "grey50", las = 1, lwd.ticks = 1)
+        # lines(dens, col = "grey30")
+        # rug(dat.hist, col = "grey75")
+        # segments( y0 = 0, y1 = 0, 
+        #     x0 = quantile(dat.hist, probs = c(0.025, 0.975))[1],
+        #     x1 = quantile(dat.hist, probs = c(0.025, 0.975))[2],
+        #     lwd = 3, col = "tomato", lend = 2)
+        # points(x = median(dat.hist), y = 0, pch = "|", cex = 3, 
+        #     col = "steelblue")
+        # txt <- paste("median =", round(median(dat.hist), 3))
+        # mtext(txt, side = 3, cex = 0.7)
 
         ## Title
         mtext(parms[i], outer = TRUE, line = -1.5, font = 2)
