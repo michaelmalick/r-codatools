2d3c5e67e491219b6f1b1247d3887bc8bbaccf68
Michael Malick <malickmj@gmail.com>
2019-03-27 17:45:26 -0700

    Update Rhat and Neff plots to take vector of values

--- R/coda_neff.R
+++ R/coda_neff.R
@@ -1,27 +1,26 @@
 #' @title Create effective sample size diagnostic graphic
 #'
 #' @description
-#'      \code{coda_neff} creates a graphic of the effective sample size for each
-#'      parameter in the input object. The left-hand y-axis is the total
-#'      effective size and the right-hand y-axis is the effective size
-#'      scaled by the total number of MCMC draws.
+#'      \code{coda_neff} creates a graphic of the effective sample size.
+#'      The left-hand y-axis is the total effective size and the right-hand
+#'      y-axis is the effective size scaled by the total number of MCMC draws.
 #'
-#' @param coda.object
-#'      an mcmc.list object
+#' @param neff
+#'      named vector of Neff values
+#'
+#' @param samps
+#'      total number of MCMC draws (N chains * N draws)
 #'
 #' @param thresh
 #'      threshold used to flag parameters with low effective sample sizes, as a
 #'      percentage of total MCMC draws
-#'
-#' @param plot
-#'      logical, should a plot be printed
 #'
 #' @param cex.labels
 #'      scaling for parameters plotted by name
 #'
 #' @return
 #'      Invisibly returns a vector of the effective sample size for each
-#'      parameter sorted from lowest (first) to highest (last). Optionally
+#'      parameter sorted from lowest (first) to highest (last) and
 #'      prints a plot of the effective sample sizes.
 #'
 #' @seealso \code{\link{effectiveSize}}
@@ -34,44 +33,40 @@
 #' library(coda)
 #' data(line)
 #'
-#' coda_neff(line)
-coda_neff <- function(coda.object, plot = TRUE,
-                      thresh = 0.5, cex.labels = 0.5) {
-
-    neff <- coda::effectiveSize(coda.object)
-
-    if(plot) {
-        samps    <- coda::niter(coda.object) * coda::nchain(coda.object)
-        ind.low  <- which(neff < thresh * samps)
-        ind.high <- which(neff >= thresh * samps)
-
-        def.par <- graphics::par(no.readonly = TRUE)
-
-        graphics::par(mar = c(3, 5, 4, 4))
-        graphics::plot(neff, ylim = c(min(neff), max(samps, neff)),
-                       type = "n",
-                       ylab = "",
-                       xlab = "",
-                       axes = FALSE,
-                       main = "Effective sample size",
-                       las = 1)
-        graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
-        graphics::box(col = "grey50")
+#' neff  <- coda::effectiveSize(line)
+#' samps <- coda::niter(line) * coda::nchain(line)
+#' coda_neff(neff, samps)
+coda_neff <- function(neff, samps, thresh = 0.5, cex.labels = 0.5) {
 
 
-        graphics::points(ind.high, neff[ind.high], pch = 16, col = "grey50")
-        if(length(ind.low) > 0)
-            graphics::text(ind.low, neff[ind.low], names(ind.low),
-                           cex = cex.labels)
-        graphics::mtext(side = 2, "Effective sample size", line = 4.0)
-        graphics::abline(h = samps, col = "grey50", lty = 2)
-        graphics::abline(h = thresh * samps, col = "red3", lty = 2)
-        txt <- paste("N draws =", samps, "   Min Neff =", floor(min(neff)))
-        graphics::mtext(txt, side = 3, cex = 0.7)
-        axt <- graphics::axTicks(side = 2)
-        graphics::axis(side = 4, at = axt, labels = axt / samps, las = 1, col = "grey50")
-        graphics::mtext(side = 4, "Neff / N", line = 3)
-        graphics::par(def.par)
-    }
+    ind.low  <- which(neff < thresh * samps)
+    ind.high <- which(neff >= thresh * samps)
+
+    def.par <- graphics::par(no.readonly = TRUE)
+
+    graphics::par(mar = c(3, 5, 4, 4))
+    graphics::plot(neff, ylim = c(min(neff), max(samps, neff)),
+                   type = "n",
+                   ylab = "",
+                   xlab = "",
+                   axes = FALSE,
+                   main = "Effective sample size",
+                   las = 1)
+    graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
+    graphics::box(col = "grey50")
+
+    graphics::points(ind.high, neff[ind.high], pch = 16, col = "grey50")
+    if(length(ind.low) > 0)
+        graphics::text(ind.low, neff[ind.low], names(ind.low),
+                       cex = cex.labels)
+    graphics::mtext(side = 2, "Effective sample size", line = 4.0)
+    graphics::abline(h = samps, col = "grey50", lty = 2)
+    graphics::abline(h = thresh * samps, col = "red3", lty = 2)
+    txt <- paste("N draws =", samps, "   Min Neff =", floor(min(neff)))
+    graphics::mtext(txt, side = 3, cex = 0.7)
+    axt <- graphics::axTicks(side = 2)
+    graphics::axis(side = 4, at = axt, labels = axt / samps, las = 1, col = "grey50")
+    graphics::mtext(side = 4, "Neff / N", line = 3)
+    graphics::par(def.par)
     invisible(sort(neff))
 }
--- R/coda_rhat.R
+++ R/coda_rhat.R
@@ -2,24 +2,21 @@
 #'
 #' @description
 #'      \code{coda_rhat} creates a graphic of the potential scale reduction
-#'      factor (Rhat) for each parameter in the input
+#'      factor (Rhat)
 #'
-#' @param coda.object
-#'      an mcmc.list object
+#' @param rhat
+#'      named vector of Rhat values
 #'
 #' @param thresh
 #'      threshold used to flag low Rhat values
-#'
-#' @param plot
-#'      logical, should a plot be printed
 #'
 #' @param cex.labels
 #'      scaling for parameters plotted by name
 #'
 #' @return
 #'      Invisibly returns a vector of the Rhat values for each
-#'      parameter sorted from highest (first) to lowest (last). Optionally
-#'      prints a plot of the effective sample sizes.
+#'      parameter sorted from highest (first) to lowest (last)
+#'      and prints a plot of the effective sample sizes.
 #'
 #' @seealso \code{\link{gelman.diag}}
 #'
@@ -31,38 +28,34 @@
 #' library(coda)
 #' data(line)
 #'
-#' coda_rhat(line)
-coda_rhat <- function(coda.object, plot = TRUE,
-                      thresh = 1.01, cex.labels = 0.5) {
+#' gd <- coda::gelman.diag(line)
+#' rhat <- gd$psrf[ , 1]
+#' coda_rhat(rhat)
+coda_rhat <- function(rhat, thresh = 1.01, cex.labels = 0.5) {
 
-    gd <- coda::gelman.diag(coda.object)
-    rhat <- gd$psrf[ , 1]
+    ind.low  <- which(rhat > thresh)
+    ind.high <- which(rhat <= thresh)
+    def.par <- graphics::par(no.readonly = TRUE)
 
-    if(plot) {
-        ind.low  <- which(rhat > thresh)
-        ind.high <- which(rhat <= thresh)
-        def.par <- graphics::par(no.readonly = TRUE)
+    graphics::par(mar = c(3, 4, 4, 2))
+    graphics::plot(rhat, ylim = c(min(rhat), max(thresh, rhat)),
+                   type = "n",
+                   ylab = "Rhat",
+                   xlab = "",
+                   axes = FALSE,
+                   main = "Potential scale reduction factor (Rhat)",
+                   las = 1)
+    graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
+    graphics::box(col = "grey50")
 
-        graphics::par(mar = c(3, 4, 4, 2))
-        graphics::plot(rhat, ylim = c(min(rhat), max(thresh, rhat)),
-                       type = "n",
-                       ylab = "Rhat",
-                       xlab = "",
-                       axes = FALSE,
-                       main = "Potential scale reduction factor (Rhat)",
-                       las = 1)
-        graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
-        graphics::box(col = "grey50")
+    graphics::points(ind.high, rhat[ind.high], pch = 16, col = "grey50")
+    if(length(ind.low) > 0)
+        graphics::text(ind.low, rhat[ind.low], names(ind.low),
+                       cex = cex.labels)
+    graphics::abline(h = thresh, col = "red3", lty = 2)
+    txt <- paste( "max Rhat =", round(max(rhat), 3))
+    graphics::mtext(txt, side = 3, cex = 0.7)
+    graphics::par(def.par)
 
-        graphics::points(ind.high, rhat[ind.high], pch = 16, col = "grey50")
-        if(length(ind.low) > 0)
-            graphics::text(ind.low, rhat[ind.low], names(ind.low),
-                           cex = cex.labels)
-        graphics::abline(h = thresh, col = "red3", lty = 2)
-        txt <- paste("chains =", coda::nchain(coda.object),
-                     "   max Rhat =", round(max(rhat), 3))
-        graphics::mtext(txt, side = 3, cex = 0.7)
-        graphics::par(def.par)
-    }
     invisible(rev(sort(rhat)))
 }
